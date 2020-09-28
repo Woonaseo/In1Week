e19b1fcc9f64680dcb549030b0748015654188ef
Woonaseo <66362366+Woonaseo@users.noreply.github.com>
2020-09-28 18:06:23 +0900

    Create 2Logical.java

---
+++ Game/2Logical.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+
+    boolean x,y,z,b;
+
+    x = false;
+    y = z =true;
+    b = x || y && z;
+
+    System.out.println("x="+x+",y="+y+",z="+z+",b="+b);
+
+
+
+  }
+}
+
+//논리 연산자
+//a && b   AND
+//a || b   OR
+//!a       NOT
